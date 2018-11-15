9de7430bd5ad3bc67c9a00adc1a7533c1a7256ce
jejoenje <jeroenminderman@gmail.com>
2018-11-15 21:04:13 +0000

    Second display now displaying voltage measured at A2. Reset calibration.

--- VoltMeter33.ino/VoltMeter33.ino.ino
+++ VoltMeter33.ino/VoltMeter33.ino.ino
@@ -4,18 +4,19 @@
 
 #include <TM1637Display.h>
 
-#define NUM_SAMPLES 50
+#define NUM_SAMPLES 100
 #define CLK1 9
 #define DIO1 8
 #define CLK2 6
 #define DIO2 5
-#define VOLT_CORRECTION 5.5
+#define VOLT_CORRECTION 6.15
 
 TM1637Display display1(CLK1, DIO1);
 TM1637Display display2(CLK2, DIO2);
 
 
-int analogInput = A1;
+int analogInput1 = A1;
+int analogInput2 = A2;
 int lightInput = A0;
 float R1 = 99500.00; // resistance of R1 (100K) 
 float R2 = 9970.00; // resistance of R2 (10K) 
@@ -23,9 +24,14 @@ float R2 = 9970.00; // resistance of R2 (10K)
 unsigned char sample_count = 0;   
 float refvcc = 0;
 float sum = 0;
-int val = 0;
-float Vout = 0.00;
-float Vin = 0.00;
+float sum1 = 0;
+float sum2 = 0;
+int val1 = 0;
+int val2 = 0;
+float Vout1 = 0.00;
+float Vout2 = 0.00;
+float Vin1 = 0.00;
+float Vin2 = 0.00;
 int whole = 0;
 int rem = 0;
 int light = 2;
@@ -69,8 +75,12 @@ int adjustBrightness() {
   return(defaultLight);
 }
 
+float getVoltage(int measurePin) {
+  
+}
+
 void setup() {
- pinMode(analogInput, INPUT); //assigning the input port
+ pinMode(analogInput1, INPUT); //assigning the input port
  pinMode(lightInput, INPUT); //assigning the input port
  Serial.begin(9600); //BaudRate
  light = adjustBrightness();
@@ -97,50 +107,53 @@ void loop() {
   sample_count = 0;
 
   while (sample_count < NUM_SAMPLES) {
-    sum += analogRead(analogInput);
+    sum1 += analogRead(analogInput1);
+    sum2 += analogRead(analogInput2);
     sample_count++;
     delay(10);
   }
   sample_count = 0;
 
-  val = float(sum)/float(NUM_SAMPLES);
-  Vout = (val * refvcc) / 1024.00; // formula for calculating voltage out i.e. V+, here 5.00
-  Vin = Vout / (R2/(R1+R2)); // formula for calculating voltage in i.e. GND
-  if (Vin<0.09)//condition 
+  val1 = float(sum1)/float(NUM_SAMPLES);
+  val2 = float(sum2)/float(NUM_SAMPLES);
+  Vout1 = (val1 * refvcc) / 1024.00; // formula for calculating voltage out i.e. V+, here 5.00
+  Vout2 = (val2 * refvcc) / 1024.00;
+  Vin1 = Vout1 / (R2/(R1+R2)); // formula for calculating voltage in i.e. GND
+  Vin2 = Vout2 / (R2/(R1+R2));
+  if (Vin1<0.09)//condition 
   {
-    Vin=0.00;//statement to quash undesired reading !
+    Vin1=0.00;//statement to quash undesired reading !
+  } 
+  if (Vin2<0.09)//condition 
+  {
+    Vin2=0.00;//statement to quash undesired reading !
   } 
 
-  Vin = Vin + (Vin/float(100))*float(VOLT_CORRECTION);
-
-  whole = Vin;
-  rem = (Vin-whole)*100;
-
+  Vin1 = Vin1 + (Vin1/float(100))*float(VOLT_CORRECTION);
+  Vin2 = Vin2 + (Vin2/float(100))*float(VOLT_CORRECTION);
+  
+  whole = Vin1;
+  rem = (Vin1-whole)*100;
   display1.showNumberDecEx(whole, (0x80 >> 3), false, 2, 0);
   display1.showNumberDecEx(rem, 0, true, 2, 2);
 
+  whole = Vin2;
+  rem = (Vin2-whole)*100;
   display2.showNumberDecEx(whole, (0x80 >> 3), false, 2, 0);
   display2.showNumberDecEx(rem, 0, true, 2, 2);
 
-  Serial.print("refvcc = ");
-  Serial.print(refvcc);
-  Serial.print(", sum = ");
-  Serial.print(sum);
-  Serial.print(", val = ");
-  Serial.print(val);
-  Serial.print(", vcc = ");
-  Serial.print(Vin);
-  Serial.print(", sensor = ");
-  Serial.print(sensorValue);
-  Serial.print(", light = ");
-  Serial.print(light);
-  Serial.println();
-
   sample_count=0;
   sum=0;
-  val=0;
+  sum1=0;
+  sum2=0;
+  val1=0;
+  val2=0;
+  Vin1=0;
+  Vin2=0;
+  Vout1=0;
+  Vout2=0;
   whole = 0;
   rem = 0;
 
-  delay(1000);
+  delay(500);
 }
